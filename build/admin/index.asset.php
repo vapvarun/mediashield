6d74ae7678cccc0a20dcc444642684888d2cd7f9
Varun Kumar Dubey <vapvarun@gmail.com>
2026-05-22 17:30:13 +0530

    Rebuild admin SPA + block bundles
    Compiled output for the source changes in this branch (timezone parsers, Videos
    search, copy-button icon, milestone separator CSS, protection-level block options,
    block duration field). `npm run build` (wp-scripts); no dependency changes.

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-notices'), 'version' => '9ea8a200ea71b382f820');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-notices'), 'version' => '1bd7586e30646164d91b');
--- build/blocks/video/index.asset.php
+++ build/blocks/video/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '159e58d4bfd72193748d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '4504730123d3f468f4b8');
